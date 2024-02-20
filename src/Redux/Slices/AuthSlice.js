81aae1ec0bb2d665c95ea0ed146f0dd00871fcad
Abhishek Kumar <abhishej7983@gmail.com>
2024-02-20 13:53:40 +0530

    Started adding basic redux setup with toolkit

---
+++ src/Redux/Slices/AuthSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isLoggedIn: localStorage.getItem('isLoggedIn') || false,
+    role: localStorage.setItem('role') || "",
+    data: localStorage.setItem('data') || {}
+};
+
+const autiSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {}
+});
+
+export const {} = autiSlice.actions;
+export default autiSlice.reducer;
